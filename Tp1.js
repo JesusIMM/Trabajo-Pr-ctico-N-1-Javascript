44f82afc377ae8d1e10f4e0b4ff693f408606309
JesusIMM <166008467+JesusIMM@users.noreply.github.com>
2024-04-04 08:14:36 -0300

    Add files via upload

---
+++ Tp1.js
@@ -0,0 +1,32 @@
+let num1 = 300
+let num2 = 240
+let suma = num1 + num2
+let resta = num1 - num2
+let división = num1 / num2
+let multiplicación = num1 * num2
+console.log ("La suma es " + (num1 + num2))
+console.log ("La resta es " + (num1 - num2))
+console.log ("La division es " + (num1 / num2))
+console.log ("La multiplicacion es " + (num1 * num2))
+let edadalumno = 2009
+let año = 2024
+alert ("Mi edad es: "+(año - edadalumno))
+
+let añonacimiento = prompt ("Cuando naciste?");
+let edad = 2024 - añonacimiento;
+alert (edad);
+
+let nombre = prompt ("Como te llamás?");
+alert ("Hola " + nombre + ", bienvenido"); 
+
+let nombre2 = prompt("Cual es tu nombre");
+let apellido = prompt("Cual es tu apellido");
+
+console.log ("Hola " + nombre2 + apellido + " bienvenido ");
+alert ("Hola " + nombre2 + apellido + " bienvenido ");
+
+let diez = 10;
+let tres = 3;
+let resultadoMultiplicacion = diez * tres;
+alert ("el resultado de la multiplicacion es " + resultadoMultiplicacion);
+
